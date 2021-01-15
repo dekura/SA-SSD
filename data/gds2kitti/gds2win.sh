524b76a8d0254f331432d0195d69d48ff957f27f
dekura <cgjhaha@qq.com>
2021-01-15 11:14:15 +0800

    ✨ feat: remove the gds2kitti to create a stand alone library.

--- data/gds2kitti/gds2win.sh
+++
@@ -1,8 +0,0 @@
-
-python=/usr/local/miniconda3/envs/gdspy/bin/python
-# case=case1
-case=case2
-
-$python gds2win.py \
---name $case \
---in_dir /Users/dekura/chen/bei/projects/pchsd/ICCAD16-N7M2EUV/$case
--- data/gds2kitti/get_hsd_poly.sh
+++
@@ -1,2 +0,0 @@
-klayout -zz -r get_hsd_poly.drc
-klayout ./results/case2/hsd_gds/testcase2_in.gds
